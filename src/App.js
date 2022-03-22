ad2910c6f3f1749e92d5e93722620a691e76c137
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-23 00:05:17 +0600

    all routes set up end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/CommonPart/Header/Header";
+import Home from "./Components/Home/Home";
+import Shop from "./Components/Shop/Shop";
+import Order from "./Components/Order/Order";
+import Inventory from "./Components/Inventory/Inventory";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="shop" element={<Shop />}></Route>
+          <Route path="order" element={<Order />}></Route>
+          <Route path="inventory" element={<Inventory />}></Route>
+          <Route path="notfound" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
